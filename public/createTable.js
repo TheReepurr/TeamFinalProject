d962eb78fb5e59f5e6ba75ea0d5f593c84f205c3
Project Nonstop <94095038+TheReepurr@users.noreply.github.com>
2023-12-14 18:50:56 -0600

    Update createTable.js

--- public/createTable.js
+++ public/createTable.js
@@ -1,116 +1,139 @@
+const createTable = (data, ignore_Id = false) => {
+  const keys = Object.keys(data[0]);
 
+  const filteredKeys = ignore_Id ? keys.filter((key) => key !== "_id") : keys;
 
-const createTable = async (data) => {
-    const keys = Object.keys(data[0]);
-    const table = document.createElement('table');
-    table.classList = ['mui-table'];
-    const thead = table.createTHead();
-    const tbody = table.createTBody();
-    const makeHeader = async () => {
-        const row = document.createElement('tr');
-        row.classList = ['course-row']
-        keys.forEach(key => {
-            const td = document.createElement('th');
-            td.innerText = key;
-            row.append(td);
-        });
-        thead.append(row);
-    }
-    await makeHeader();
-    data.forEach(async d => {
-        const row = document.createElement('tr');
-        row.dataset.id = d._id;
-        row.classList = ['course-row']
-        for (let key of keys) {
-            const td = document.createElement('td');
-            td.innerText = d[key];
-            row.append(td);
-        }
-        tbody.append(row);
+  const table = document.createElement("table");
+  table.classList.add("mui-table"); // Use add() method to add a class
+
+  const thead = table.createTHead();
+  const tbody = table.createTBody();
+
+  const makeHeader = () => {
+    const row = document.createElement("tr");
+    row.classList.add("course-row"); // Use add() method to add a class
+    filteredKeys.forEach((key) => {
+      const th = document.createElement("th");
+      th.innerText = key;
+      row.appendChild(th); // Use appendChild() method to append child elements
     });
-    return table;
-}
+    thead.appendChild(row); // Use appendChild() method to append child elements
+  };
 
+  makeHeader();
+
+  data.forEach((d) => {
+    const row = document.createElement("tr");
+    row.dataset.id = d._id;
+    row.classList.add("course-row"); // Use add() method to add a class
+    filteredKeys.forEach((key) => {
+      const td = document.createElement("td");
+      td.innerText = d[key];
+      row.appendChild(td); // Use appendChild() method to append child elements
+    });
+    tbody.appendChild(row); // Use appendChild() method to append child elements
+  });
+
+  return table;
+};
 
 const teacherAddButtons = (table) => {
-    const thead = table.querySelector('thead');
-    const headerRow = thead.querySelector('tr');
-    headerRow.append(document.createElement('th'));
+  const thead = table.querySelector("thead");
+  const headerRow = thead.querySelector("tr");
+  headerRow.appendChild(document.createElement("th")); // Use appendChild() method to append child elements
 
-    const tbody = table.querySelector('tbody');
-    const rows = tbody.querySelectorAll('tr');
-    for (let row of rows) {
-        const td = document.createElement('td');
-        const btns = ["Edit", "Delete"].map(name => {
-            const btn = document.createElement('button');
-            btn.innerText = name;
-            btn.classList = ['button modern-button']
-            return btn;
+  const tbody = table.querySelector("tbody");
+  const rows = tbody.querySelectorAll("tr");
+  for (let row of rows) {
+    const td = document.createElement("td");
+    const btns = ["Edit", "Delete"].map((name) => {
+      const btn = document.createElement("button");
+      btn.innerText = name;
+      btn.classList.add("button", "modern-button"); // Use add() method to add classes
+      return btn;
+    });
+    btns[0].onclick = async () => {
+      sessionStorage.setItem("teacherId", row.dataset.id);
+      window.location.assign(`${window.location.origin}/teacherForm`); // Use template literal for string concatenation
+    };
+    btns[1].onclick = async () => {
+      alert(row.dataset.id);
+      axiosDelete(`./api/teacher/${JSON.stringify(row.dataset.id)}`)
+        .then((res) => {
+          console.log(res);
+          if (res.status === 200) {
+            alert("Teacher deleted successfully");
+            // Add code to update your UI or handle the deletion in the client-side
+          } else {
+            alert("Failed to delete teacher");
+          }
+        })
+        .catch((e) => {
+          console.error(e);
+          alert("An error occurred while deleting teacher");
         });
-        btns[0].onclick = async () => {
-            sessionStorage.setItem('teacherId', row.dataset.id)
-            window.location.assign(window.location.origin + '/teacherForm');
-        }
-        btns[1].onclick = async () => {
-            alert(row.dataset.id);
-            axiosDelete(`./api/teacher/${JSON.stringify(row.dataset.id)}`)
-                .then(res => {
-                    console.log(res);
-                    if (res.status === 200) {
-                        alert('Teacher deleted successfully');
-                        // Add code to update your UI or handle the deletion in the client-side
-                    } else {
-                        alert('Failed to delete teacher');
-                    }
-                })
-                .catch(e => {
-                    console.error(e);
-                    alert('An error occurred while deleting teacher');
-                });
 
-            location.reload();
-        }
-        btns.forEach(btn => td.append(btn));
-        row.append(td)
-    }
-}
+      location.reload();
+    };
+    btns.forEach((btn) => td.appendChild(btn)); // Use appendChild() method to append child elements
+    row.appendChild(td); // Use appendChild() method to append child elements
+  }
+};
 
 const courseAddButtons = (table) => {
-    const thead = table.querySelector('thead');
-    const headerRow = thead.querySelector('tr');
-    headerRow.append(document.createElement('th'));
+  const thead = table.querySelector("thead");
+  const headerRow = thead.querySelector("tr");
+  headerRow.appendChild(document.createElement("th")); // Use appendChild() method to append child elements
 
-    const tbody = table.querySelector('tbody');
-    const rows = tbody.querySelectorAll('tr');
-    for (let row of rows) {
-        const td = document.createElement('td');
-        const btns = ["Edit", "Delete"].map(name => {
-            const btn = document.createElement('button');
-            btn.innerText = name;
-            btn.classList = ['button modern-button']
-            return btn;
-        });
-        btns[0].onclick = async () => {
-            sessionStorage.setItem('courseId', row.dataset.id)
-            window.location.assign(window.location.origin + '/courseForm');
+  const tbody = table.querySelector("tbody");
+  const rows = tbody.querySelectorAll("tr");
+  for (let row of rows) {
+    const td = document.createElement("td");
+    const btns = ["Edit", "Delete", "Sign Up"].map((name) => {
+      const btn = document.createElement("button");
+      btn.innerText = name;
+      btn.classList.add("button", "modern-button"); // Use add() method to add classes
+      return btn;
+    });
+    btns[0].onclick = async () => {
+      sessionStorage.setItem("courseId", row.dataset.id);
+      window.location.assign(`${window.location.origin}/courseForm`); // Use template literal for string concatenation
+    };
+    btns[1].onclick = async () => {
+      try {
+        const response = await axiosDelete(`../api/course/${JSON.stringify(row.dataset.id)}`);
+        if (response.status === 200) {
+          alert("Course deleted successfully");
+          // Add code to update your UI or handle the deletion in the client-side
+        } else {
+          alert("Failed to delete course");
         }
-        btns[1].onclick = async () => {
-            try {
-                const response = await axiosDelete(`../api/course/${JSON.stringify(row.dataset.id)}`);
-                if (response.status === 200) {
-                    alert('Course deleted successfully');
-                    // Add code to update your UI or handle the deletion in the client-side
-                } else {
-                    alert('Failed to delete course');
-                }
-            } catch (error) {
-                console.error(error);
-                alert('An error occurred while deleting course');
-            }
-            location.reload();
-        };
+      } catch (error) {
+        console.error(error);
+        alert("An error occurred while deleting course");
+      }
+      location.reload();
+    };
+    btns[2].onclick = async () => {
+      try {
+        const response = await axiosPut(
+          `../api/signUpForCourse`,
+          { courseId: row.dataset.id, userId: JSON.parse(localStorage.getItem("User")).token }
+        );
+        if (response.status === 200) {
+          alert("Course Added");
+          // Add code to update your UI or handle the deletion in the client-side
+        } else {
+          alert("Something went wrong signing up for this course!");
+        }
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+      location.reload();
+    };
 
-        btns.forEach(btn => td.append(btn));
-        row.append(td)
-    }
-}
+    btns.forEach((btn) => td.appendChild(btn)); // Use appendChild() method to append child elements
+    row.appendChild(td); // Use appendChild() method to append child elements
+  }
+};
